ed440a8a736e8b8b4a64a63f2b30b542d240a301
solaire-pc <contact@unharmfulhazard.com>
2023-10-29 23:04:25 -0300

    Removed an error that was raised due to the script not preserving the environment when running cargo with sudo

--- installer.sh
+++ installer.sh
@@ -10,9 +10,13 @@ fi
 CURRENT_DIRECTORY="$(pwd)"
 CARGO_HOME="/home/$ORIGINAL_USER/.cargo"
 RUSTUP_HOME="/home/$ORIGINAL_USER/.rustup"
+CARGO_BUILD="/home/$ORIGINAL_USER/.cargo/bin/cargo build"
 
 # Use the -E option to preserve the environment when running cargo with sudo
-sudo -E -u "$ORIGINAL_USER" CARGO_HOME="$CARGO_HOME" RUSTUP_HOME="$RUSTUP_HOME" /home/"$ORIGINAL_USER"/.cargo/bin/cargo build
+sudo -E -u "$ORIGINAL_USER" sh -c "export CARGO_HOME='$CARGO_HOME' && export RUSTUP_HOME='$RUSTUP_HOME' && $CARGO_BUILD"
+
+# Rename password_generator to passgen
+mv "$CURRENT_DIRECTORY"/target/debug/password_generator "$CURRENT_DIRECTORY"/target/debug/passgen
 
 # Move the binary to the user's bin directory
-sudo mv "$CURRENT_DIRECTORY"/target/debug/password_generator /bin/
+mv "$CURRENT_DIRECTORY"/target/debug/passgen /usr/local/bin/ --force
