f9405b267588896d5e7fe6b920a8dedab46a4f60
solaire-pc <contact@unharmfulhazard.com>
2023-10-29 21:48:14 -0300

    Added functions that check the repetition of characters in the string

--- src/characters.rs
+++ src/characters.rs
@@ -1,3 +1,4 @@
+use std::arch::x86_64::_mulx_u32;
 use rand::Rng; // generates random values
 
 pub struct PreviousCharacters {
@@ -9,6 +10,10 @@ impl PreviousCharacters {
         PreviousCharacters {
             characters: (-1, -1, -1)
         }
+    }
+
+    pub fn get_characters(&self) -> (i8, i8, i8) {
+        self.characters
     }
 
     pub fn adjust(&mut self, last: i8) {
@@ -24,4 +29,26 @@ pub fn get_char_type() -> usize {
 pub fn get_char(str: &str, str_len: usize) -> &str {
     let pos = rand::thread_rng().gen_range(0..str_len);
     &str[pos..pos + 1]
+}
+
+pub fn check_repetition(new: i8, last: (i8, i8, i8)) -> i8 {
+    let mut repetitions: i8 = 0;
+    if last.0 == last.1 {
+        repetitions += 1;
+    }
+    if last.1 == last.2 {
+        repetitions += 1;
+    }
+    if last.0 == last.2 {
+        repetitions += 1;
+    }
+    repetitions
+}
+
+pub fn reroll(new: i8, last: (i8, i8, i8)) -> bool {
+    let repetitions = check_repetition(new, last);
+    if repetitions >= 2 {
+        return true;
+    }
+    false
 }
